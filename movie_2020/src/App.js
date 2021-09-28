be260b6464119ba94118ef5d831a34c7bc97e215
maie421 <maie421@naver.com>
2021-09-28 23:33:11 +0900

    영화 데이터 가져오기

--- movie_2020/src/App.js
+++ movie_2020/src/App.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return <div>hello!!</div>
+
+class App extends React.Component {
+
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    this.setState({ movies, isLoading: false });
+  };
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading} = this.state;
+    
+    return <div>{isLoading ? 'Loading...' : 'we are ready'}</div>
+  }
 }
 
 export default App;
