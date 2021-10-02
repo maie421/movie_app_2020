136b80103735302be1f4cf0a78a0b33b49a9737f
maie421 <maie421@naver.com>
2021-10-02 15:52:04 +0900

    마무리

---
+++ movie_2020/src/routes/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+  return (
+    <div className="about__container">
+      <span>
+        ~~~
+      </span>
+      <span>~~~</span>
+    </div>
+  );
+}
+
+export default About;
